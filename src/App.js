bcdcbcc293e83bc5e73fdadba6c2504423c1807d
bonisiweGabela <142170923+bonisiweGabela@users.noreply.github.com>
2023-11-19 18:47:10 +0200

    edited and styled the link

--- src/App.js
+++ src/App.js
@@ -5,7 +5,16 @@ export default function App() {
   return (
     <div className="App">
       <h1> Weather App</h1>
+
       <WeatherApp />
+
+      <footer>
+        {" "}
+        <a href="https://github.com/bonisiweGabela/weather-app-react">
+          Open-source code
+        </a>{" "}
+        by Bonisiwe Gabela
+      </footer>
     </div>
   );
 }
